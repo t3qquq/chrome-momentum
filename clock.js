c21c8ee77e3346c7591b4c957780d9af1f272433
shin-jaewon <shine970530@gmail.com>
2020-10-30 13:01:12 +0900

    all about times 完

--- clock.js
+++ clock.js
@@ -1,21 +1,56 @@
 const clockContainer = document.querySelector('.js-clock');
 const clockTitle = clockContainer.querySelector('h1');
 const AMPM = clockContainer.querySelector('h2');
+const dayInfo = document.querySelector('.js-day');
 
 function getTime(){
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
- 
+    const time = new Date();
+    const minutes = time.getMinutes();
+    const hours = time.getHours();
+    const seconds = time.getSeconds();
+
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const year = time.getFullYear();
+
+    if(hours===12){
+        clockTitle.innerText= `12 : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10? `0${seconds}`:seconds}`
+    }
+    else if(hours===24){
+        clockTitle.innerText= `0 : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10? `0${seconds}`:seconds}`
+    }else{
+        clockTitle.innerText= `${hours<12?hours:hours-12} : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10? `0${seconds}`:seconds}`
+    }
+
     if(hours<12 || hours===24)
-    AMPM.innerText = "AM";
+    AMPM.innerText = "오전";
     else
-    AMPM.innerText = "PM";
-
-
-    clockTitle.innerText= `${hours<12?hours:hours-12} : ${minutes<10 ? `0${minutes}`:minutes} : ${seconds<10? `0${seconds}`:seconds}`
-
+    AMPM.innerText = "오후";
+    
+    switch(day){
+        case 0:
+            dayInfo.innerText = `${month+1}월 ${date}일 일요일, ${year}`;
+            break;
+        case 1:
+            dayInfo.innerText = `${month+1}월 ${date}일 월요일, ${year}`;
+            break;
+        case 2:
+            dayInfo.innerText = `${month+1}월 ${date}일 화요일, ${year}`;
+            break;
+        case 3:
+            dayInfo.innerText = `${month+1}월 ${date}일 수요일, ${year}`;
+            break;
+        case 4:
+            dayInfo.innerText = `${month+1}월 ${date}일 목요일, ${year}`;
+            break;
+        case 5:
+            dayInfo.innerText = `${month+1}월 ${date}일 금요일, ${year}`;
+            break;
+        case 6:
+            dayInfo.innerText = `${month+1}월 ${date}일 토요일, ${year}`;
+            break;
+    }
 }
 
 function init(){
